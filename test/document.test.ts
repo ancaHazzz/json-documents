867637e9be0f2e20ed8519a545e7cc402fe4296c
Anca Hariga <anca.hariga@kombo.co>
2020-02-24 15:35:51 +0100

    Add another test

--- test/document.test.ts
+++ test/document.test.ts
@@ -1,12 +1,14 @@
 import { init } from '../src/service/Database'
-import { storeDocument, getDocumentVersion } from '../src/controller/DocumentManager'
+import {
+  storeDocument,
+  getDocumentVersion,
+  getDocumentHistory
+} from '../src/controller/DocumentManager'
 
 const JSON_TEMPLATE = {
-  document: {
-    property1: 'value1',
-    property2: 'value2',
-    property3: 'value3'
-  }
+  property1: 'value1',
+  property2: 'value2',
+  property3: 'value3'
 }
 
 describe('test sncf operations', () => {
@@ -24,6 +26,28 @@ describe('test sncf operations', () => {
     expect(savedDocument.content).toEqual(JSON_TEMPLATE)
     done()
   })
-  
 
+  test('store multiple versions & get document history', async done => {
+    const docId = 'multi-123-abc'
+    let content1: any = {},
+      content2: any = {}
+    const v0 = await storeDocument(new Date(), docId, JSON_TEMPLATE)
+
+    Object.assign(content1, JSON_TEMPLATE)
+    content1!.property1 = 'changed value'
+    const v1 = await storeDocument(new Date(), docId, content1)
+
+    Object.assign(content2, JSON_TEMPLATE)
+    content2!.property1 = 'super changed value'
+    content2!.property2 = 'another changed value'
+    const v2 = await storeDocument(new Date(), docId, content2)
+
+    const docHistory = await getDocumentHistory(docId)
+    expect(docHistory).toBeTruthy()
+    expect(docHistory?.length).toEqual(3)
+    expect(docHistory![0].version).not.toEqual(docHistory![1].version)
+    expect(docHistory![0].content).not.toEqual(docHistory![1].content)
+    expect(docHistory![0].document).toEqual(docHistory![1].document)
+    done()
+  })
 })
